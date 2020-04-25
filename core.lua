e2d7305cdb8d68f8207db03326696fb585d8f859
mtimo101289@gmail.com <mtimo101289@gmail.com>
2020-04-25 12:45:36 +0200

    add helper for finding first unrolled

--- core.lua
+++ core.lua
@@ -1,27 +1,6 @@
 local addonName, helpers = ...;
 
 
-local trackedItems = {
-    "Zulian Coin",
-    "Razzashi Coin",
-    "Hakkari Coin",
-    "Sandfury Coin",
-    "Skullsplitter Coin",
-    "Bloodscalp Coin",
-    "Gurubashi Coin",
-    "Vilebranch Coin",
-    "Witherbark Coin",
-    "Red Hakkari Bijou",
-    "Green Hakkari Bijou",
-    "Blue Hakkari Bijou",
-    "Purple Hakkari Bijou",
-    "Bronze Hakkari Bijou",
-    "Silver Hakkari Bijou",
-    "Gold Hakkari Bijou",
-    "Orange Hakkari Bijou",
-    "Yellow Hakkari Bijou"
-};
-
 local function tableIncludes (tab, val)
     for index, value in ipairs(tab) do
         if value == val then
@@ -35,9 +14,6 @@ end
 local name, realm = UnitName("player");
 local f = CreateFrame("frame");
 
-local itemResults;
-local rollResults;
-
 f:RegisterEvent("CHAT_MSG_SYSTEM");
 f:RegisterEvent("LOOT_OPENED");
 f:SetScript("OnEvent", function(self, event, ...)
@@ -49,27 +25,9 @@ f:SetScript("OnEvent", function(self, event, ...)
       local message = ...;
       local author, rollResult, rollMin, rollMax = string.match(message, "(.+) rolls (%d+) %((%d+)-(%d+)%)");
       if author and author == name then
-        local raidMemberName = GetRaidRosterInfo(rollResult);
-        table.insert(rollResults, { id = rollResult, name = raidMemberName });
-        
-        if #itemResults == #rollResults then 
-            for lootIndex, lootWrapper in ipairs(lootInfo) do
-                if tableIncludes(trackedItems, lootWrapper.item) then
-                    local poppedRaidResult = table.remove(rollResults, 1);
-
-                    for i=1,GetNumGroupMembers() do
-                        local candidate = GetMasterLootCandidate(lootIndex, i);
-                        if candidate and poppedRaidResult.name and candidate == poppedRaidResult.name then
-                            SendChatMessage("<RepRoller> Assigning " .. lootWrapper.item .. " to raid member " .. poppedRaidResult.id .. "(" .. poppedRaidResult.name .. ")", "RAID");
-                            --GiveMasterLoot(lootIndex, poppedRaidResult.id);
-                        else
-                            SendChatMessage("<RepRoller> Error assigning " .. lootWrapper.item .. " to raid member " .. poppedRaidResult.id .. "(" .. poppedRaidResult.name .. ")", "RAID");
-                        end
-                    end
-                    
-                end
-            end
-        end
+        --local raidMemberName = GetRaidRosterInfo(rollResult);
+        --table.insert(rollResults, { id = rollResult, name = raidMemberName });
+        helpers.handleRoll()
       end
     end
 end)
--- helpers.lua
+++ helpers.lua
@@ -21,8 +21,12 @@ helpers.resetStorage = function()
             "Gold Hakkari Bijou",
             "Orange Hakkari Bijou",
             "Yellow Hakkari Bijou",
-            "Linked Chain Cloak"
-        }
+            "Linked Chain Cloak",
+            "Rawhide Boots",
+            "Searing Blade",
+            "Clay Ring of the Gorilla"
+        },
+        rolls = {}
     };
 end
 
@@ -38,13 +42,27 @@ helpers.tableIncludes = function(tab, val)
     return false;
 end
 
+helpers.firstUnrolled = function()
+    for k,v in ipairs(helpers.storage.rolls) do
+        if v.rollValue == nil then return v end;
+    end
+end
+
 helpers.handleLootOpened = function()
     local lootInfo = GetLootInfo();
     for lootIndex, lootWrapper in ipairs(lootInfo) do
         if helpers.tableIncludes(helpers.storage.trackedItems, lootWrapper.item) then
+            table.insert(helpers.storage.rolls, {lootIndex = lootIndex, lootName = lootWrapper.item});
             RandomRoll(1, GetNumGroupMembers());
         end
     end
+
+    print(#helpers.storage.rolls)
+end
+
+helpers.handleRoll = function()
+    local firstUnrolled = helpers.firstUnrolled();
+    print(firstUnrolled.lootName)
 end
 
 
